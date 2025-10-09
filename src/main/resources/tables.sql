c24e6cc6396f24c1de990125de0bfe25599d3ddb
victor <vgm@gmail.com>
2025-10-09 10:55:05 +0200

    Add tables EspeceMonstre and Zone to database script

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -3,3 +3,31 @@ CREATE TABLE Entraineurs(
                             id INTEGER PRIMARY KEY AUTO_INCREMENT,
                             nom VARCHAR(255),
                             argents INTEGER);
+
+-- Creation de la table EspeceMonstre
+CREATE TABLE EspeceMonstre(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255),
+    type VARCHAR(255),
+    baseAttaque INTEGER,
+    baseDefense INTEGER,
+    baseVitesse INTEGER,
+    baseAttaqueSpe INTEGER,
+    baseDefenseSpe INTEGER,
+    basePv INTEGER,
+    modAttaque INTEGER,
+    modDefense INTEGER,
+    modVitesse INTEGER,
+    modAttaqueSpe INTEGER,
+    modDefenseSpe INTEGER,
+    modPv INTEGER,
+    description VARCHAR(255),
+    particularite VARCHAR(255),
+    caracteres VARCHAR(255);
+
+-- Creation de la table Zone
+CREATE TABLE Zone(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255),
+    expZone INTEGER);
+
