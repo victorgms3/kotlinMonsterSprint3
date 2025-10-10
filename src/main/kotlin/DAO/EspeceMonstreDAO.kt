2dbd91b393f7073b1294116b271e8e85b9bd00e8
victor <vgm@gmail.com>
2025-10-10 16:25:39 +0200

    Ajout de la méthode `findByNom` dans `EspeceMonstreDAO` pour rechercher une espèce par son nom et correction de la requête dans `findById`.

--- src/main/kotlin/DAO/EspeceMonstreDAO.kt
+++ src/main/kotlin/DAO/EspeceMonstreDAO.kt
@@ -82,7 +82,7 @@ class EspeceMonstreDAO(val bdd: BDD = db) {
      */
     fun findById(id: Int): EspeceMonstre? {
         var result: EspeceMonstre? = null
-        val sql = "SELECT * FROM Entraineurs WHERE id = ?"
+        val sql = "SELECT * FROM EspeceMonstre WHERE id = ?"
         val requetePreparer = bdd.connectionBDD!!.prepareStatement(sql)
         requetePreparer.setInt(1, id) // insere la valeur de l'id dans la requete preparer
         val resultatRequete = bdd.executePreparedStatement(requetePreparer)
@@ -129,4 +129,63 @@ class EspeceMonstreDAO(val bdd: BDD = db) {
         requetePreparer.close()
         return result
     }
+    /**
+     * Recherche une espèce par son nom.
+     *
+     * @param nomRechercher Le nom de l'espèce à rechercher.
+     * @return Une liste d'espèces correspondant au nom donné.
+     */
+    fun findByNom(nomRechercher: String): MutableList<EspeceMonstre> {
+        val result = mutableListOf<EspeceMonstre>()
+        val sql = "SELECT * FROM EspeceMonstre WHERE nom = ?"
+        val requetePreparer = bdd.connectionBDD!!.prepareStatement(sql)
+        requetePreparer.setString(1, nomRechercher)
+        val resultatRequete = bdd.executePreparedStatement(requetePreparer)
+
+        if (resultatRequete != null) {
+            while (resultatRequete.next()) {
+                val id = resultatRequete.getInt("id")
+                val nom = resultatRequete.getString("nom")
+                val type = resultatRequete.getString("type")
+                val baseAttaque = resultatRequete.getInt("baseAttaque")
+                val baseDefense = resultatRequete.getInt("baseDefense")
+                val baseVitesse = resultatRequete.getInt("baseVitesse")
+                val baseAttaqueSpe = resultatRequete.getInt("baseAttaqueSpe")
+                val baseDefenseSpe = resultatRequete.getInt("baseDefenseSpe")
+                val basePv = resultatRequete.getInt("basePv")
+                val modAttaque = resultatRequete.getDouble("modAttaque")
+                val modDefense = resultatRequete.getDouble("modDefense")
+                val modVitesse = resultatRequete.getDouble("modVitesse")
+                val modAttaqueSpe = resultatRequete.getDouble("modAttaqueSpe")
+                val modDefenseSpe = resultatRequete.getDouble("modDefenseSpe")
+                val modPv = resultatRequete.getDouble("modPv")
+                val description = resultatRequete.getString("description")
+                val particularites = resultatRequete.getString("particularites")
+                val caracteres = resultatRequete.getString("caractere")
+                result.add(EspeceMonstre(id,
+                    nom,
+                    type,
+                    baseAttaque,
+                    baseDefense,
+                    baseVitesse,
+                    baseAttaqueSpe,
+                    baseDefenseSpe,
+                    basePv,
+                    modAttaque,
+                    modDefense,
+                    modVitesse,
+                    modAttaqueSpe,
+                    modDefenseSpe,
+                    modPv,
+                    description,
+                    particularites,
+                    caracteres))
+            }
+        }
+
+        requetePreparer.close()
+        return result
+    }
+
+
 }
