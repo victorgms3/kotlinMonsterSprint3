a98195da08ec2e4d05324b7125e74887cf4b19e2
victor <vgm@gmail.com>
2025-10-10 17:21:38 +0200

    Ajout de la méthode `findAll` dans `IndividuMonstreDAO` pour récupérer tous les individus monstres depuis la base de données.

--- src/main/kotlin/DAO/IndividuMonstreDAO.kt
+++ src/main/kotlin/DAO/IndividuMonstreDAO.kt
@@ -2,6 +2,7 @@ package org.example.DAO
 
 import BDD
 import org.example.db
+import org.example.dresseur.Entraineur
 import org.example.monstre.IndividuMonstre
 import java.sql.PreparedStatement
 import java.sql.SQLException
@@ -17,4 +18,25 @@ import java.sql.Statement
  * @param bdd L'objet de connexion à la base de données.
  */
 class IndividuMonstreDAO (val bdd: BDD = db){
+    fun findAll(): MutableList<IndividuMonstre> {
+        val result = mutableListOf<IndividuMonstre>()
+        val sql = "SELECT * FROM IndividuMonstre"
+        val requetePreparer = bdd.connectionBDD!!.prepareStatement(sql)
+        val resultatRequete = bdd.executePreparedStatement(requetePreparer)
+
+        if (resultatRequete != null) {
+            while (resultatRequete.next()) {
+                val id = resultatRequete.getInt("id")
+                val nom = resultatRequete.getString("nom")
+                val argents = resultatRequete.getInt("argents")
+                result.add(Entraineur(id, nom, argents))
+                /**
+                 * a completer au dessus avec les autres champs de la table IndividuMonstre
+                 */
+            }
+        }
+
+        requetePreparer.close()
+        return result
+    }
 }
